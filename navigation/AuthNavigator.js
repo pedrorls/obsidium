a8d7b040914ce920bf4e09cadd76aa88d4cea9c2
Pedro Silva <pedro.rls93@gmail.com>
2019-03-20 15:29:57 -0300

    added signup auth navigation

--- navigation/AuthNavigator.js
+++ navigation/AuthNavigator.js
@@ -2,9 +2,11 @@ import React from 'react';
 import {createStackNavigator, } from 'react-navigation'
 
 import LoginScreen from '../screens/LoginScreen'
+import SignUpScreen from '../screens/SignUpScreen';
 
 const AuthStack = createStackNavigator({
-  Login: LoginScreen 
+  Login: LoginScreen,
+  Signup: SignUpScreen,
 });
 
 export default AuthStack;
