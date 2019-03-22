2ad834c329ce8f6966993cad9361fa4257c4731d
Pedro Silva <pedro.rls93@gmail.com>
2019-03-21 23:43:00 -0300

    added button to create post

--- screens/FeedScreen.js
+++ screens/FeedScreen.js
@@ -1,11 +1,17 @@
 import React from 'react';
-
+import { Button } from 'react-native-elements'; 
 import Feed from '../components/Feed';
 
 
 export default class FeedScreen extends React.Component {
   static navigationOptions = {
     title: 'Feed',
+    headerRight: (
+      <Button
+        onPress={() => alert('This is a button!')}
+        title="Criar Post"
+        buttonStyle={{ backgroundColor: '#FDA50F', marginRight: 14 }}
+      />)
   };
 
   onSupportPress() {
