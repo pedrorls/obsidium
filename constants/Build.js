0217a38935fff82d00932cbc4470c955563347b3
Pedro Silva <pedro.rls93@gmail.com>
2019-03-26 01:11:59 -0300

    create constants for pickers

---
+++ constants/Build.js
@@ -0,0 +1,9 @@
+const Build = {
+  "Centro": "Escolha o centro",
+  "CBB": "Centro de Biociências e Biotecnologia",
+  "CCTA": "Centro de Ciências e Tecnologias Agropecuárias",
+  "CCH": "Centro de Ciências do Homem",
+  "CCT": "Centro de Ciência e Tecnologia"
+}
+
+export default Build;
---
+++ constants/Course.js
@@ -0,0 +1,21 @@
+const Course = {
+  0: "Escolha o curso",
+  1: "Administração Pública",
+  2: "Agrono­mia",
+  3: "Biologia (licenciatura)",
+  4: "Ciência da Computação",
+  5: "Pedagogia (licenciatura)",
+  6: "Ciências Biológicas (bacharelado)",
+  7: "Ciências Sociais",
+  8: "Engenharia Civil",
+  9: "Engenharia de Exploração e Produção de Petróleo",
+  10: "Engenharia de Produção",
+  11: "Engenharia Metalúrgica",
+  12: "Física (licenciatura)",
+  13: "Matemática (licenciatura)",
+  14: "Medicina Veterinária",
+  15: "Química (licenciatura)",
+  15: "Zootecnia",
+}
+
+export default Course;
