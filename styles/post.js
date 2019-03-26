1b3e3f45bd3bee9e69a495e7c74dcad13ec2900e
Pedro Silva <pedro.rls93@gmail.com>
2019-03-26 00:29:16 -0300

    styles folder

---
+++ styles/post.js
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  containerStyle: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around'
+  },
+  picker: {
+    height: 50,
+    width: 350,
+  },
+  input: {
+    height: 200,
+    borderWidth: 1,
+    borderColor: '#fff'
+  },
+  button: {
+    width: 300,
+    height: 50,
+    borderRadius: 10,
+    backgroundColor: '#FDA50F',
+    alignSelf: 'center'
+  },
+});
+
+export default styles;
---
+++ styles/textarea.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    borderColor: '#dedede',
+    borderWidth: 1,
+    margin: 7,
+    borderRadius: 10,
+    padding: 5
+  },
+  textarea: {
+    height: 150,
+    justifyContent: 'flex-start'
+  }
+});
+
+export default styles;
