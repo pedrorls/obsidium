9e967301bc1fb42d80598d023ebf8bf325519e74
Pedro Silva <pedro.rls93@gmail.com>
2019-04-29 18:25:26 -0300

    text for explanations

---
+++ constants/Explanations.js
@@ -0,0 +1,16 @@
+const moral_what = "Assédio moral é toda conduta abusiva que, intencional e frequentemente, fira a dignidade e a integridade física ou psíquica de uma pessoa, ameaçando seu emprego ou destruindo o clima de trabalho. É caracterizado por comportamentos abusivos e humilhantes frequentes, expressos por gestos, palavras e atitudes que prejudiquem a integridade física e mental do empregado. Também pode ser verificado por cobrança de metas abusivas por parte de superiores hierárquicos. Os apelidos e conotações pejorativas são exemplos comuns de assédio moral."
+const moral_carac = "Alguns exemplos que caracterizam o assédio moral são críticas públicas ao trabalhador, tendendo para a humilhação; tratar o trabalhador por um apelido pejorativo; fazer brincadeiras e piadas envolvendo o assediado; solicitar tarefas abaixo ou acima da qualificação do trabalhador ou mandar que o empregado faça tarefas inúteis ou irrealizáveis"
+const moral_denun = "A vítima pode procurar os recursos humanos da empresa; a ouvidoria; o sindicato da categoria, registrar boletim de ocorrência na Delegacia e nas Superintendências Regionais do Trabalho; Ministério Público do Trabalho."
+
+const sexual_what = "O assédio sexual é todo o comportamento indesejado de caráter sexual, sob forma verbal, não verbal ou física, com o objetivo ou o efeito de perturbar ou constranger a pessoa, afetar a sua dignidade, ou de lhe criar um ambiente intimidativo, hostil, degradante, humilhante ou desestabilizador. Está tipificado no artigo 216 – A, caput, do Código Penal, o crime de assédio sexual, que consiste no fato de o agente “constranger alguém com o intuito de obter vantagem ou favorecimento sexual, prevalecendo-se o agente de sua condição de superior hierárquico ou ascendência inerentes ao exercício de emprego, cargo ou função”"
+const sexual_carac = "Para caracterizar o crime de assédio sexual, o constrangimento pode ser realizado verbalmente, por escrito ou gestos. Se houver emprego de violência ou grave ameaça, o delito será o de estupro (CP, art. 213)."
+const sexual_denun = "Assim como o assédio moral, a vítima pode procurar os recursos Humanos do órgão; a ouvidoria; o sindicato da categoria, registrar boletim de ocorrência na Delegacia e nas Superintendências Regionais do Trabalho; Ministério Público do Trabalho."
+
+export {
+  moral_what,
+  moral_carac,
+  moral_denun,
+  sexual_what,
+  sexual_denun,
+  sexual_carac
+}
